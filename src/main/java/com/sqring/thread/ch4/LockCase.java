5aee84c2f4277dd4e4c54fc1aa6a6db291e8b1e7
chaohaibo <408100209@qq.com>
2021-05-23 23:20:42 +0800

    使用Lock的范例

---
+++ src/main/java/com/sqring/thread/ch4/LockCase.java
@@ -0,0 +1,72 @@
+package com.sqring.thread.ch4;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 描述: 使用Lock的范例
+ *
+ * @author www.zhouwenfang.com
+ * @version 1.0
+ * @date 2021/05/23 20:24:06
+ */
+public class LockCase {
+
+    private Lock lock = new ReentrantLock();
+
+    private int age = 100000;//初始100000
+
+    private static class TestThread extends Thread {
+
+        private LockCase lockCase;
+
+        public TestThread(LockCase lockCase, String name) {
+            super(name);
+            this.lockCase = lockCase;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100000; i++) {//递增100000
+                lockCase.test();
+            }
+            System.out.println(Thread.currentThread().getName()
+                    + " age =  " + lockCase.getAge());
+        }
+    }
+
+    public void test() {
+        lock.lock();
+        try {
+            age++;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void test2() {
+        lock.lock();
+        try {
+            age--;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        LockCase lockCase = new LockCase();
+        Thread endThread = new TestThread(lockCase, "endThread");
+        endThread.start();
+        for (int i = 0; i < 100000; i++) {//递减100000
+            lockCase.test2();
+        }
+        System.out.println(Thread.currentThread().getName()
+                + " age =  " + lockCase.getAge());
+
+    }
+}
